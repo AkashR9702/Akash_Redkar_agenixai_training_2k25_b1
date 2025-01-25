6370d75c89588a6820cee350f9ecad33a9a9a709
Akash Redkar <152199489+AkashR9702@users.noreply.github.com>
2025-01-25 23:55:30 +0530

    Delete SQL_Week-1_Library management Assignment/Q4all customers who have overdue books.sql

--- SQL_Week-1_Library management Assignment/Q4all customers who have overdue books.sql
+++
@@ -1,9 +0,0 @@
---List all customers who have overdue books 
---(assume overdue if ReturnDate is null and IssueDate is older than 30 days).
---Explain
-Select  Distinct c.customername
-From customer c 
-Join transaction t 
-on c.customerid = t.customerid
-Where t.returndate Is NULL and t.issuedate <  Current_date - Interval '30 days'
-
