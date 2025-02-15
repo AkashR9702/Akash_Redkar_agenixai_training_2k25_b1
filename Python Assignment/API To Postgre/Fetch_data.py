00bf57b0657aa29f175f9245d8003b9bd89331ea
Akash Redkar <aakash.redkar9702@gmail.com>
2025-02-15 20:52:36 +0530

    Task 1: API to PostgreSQL implemented and added

---
+++ Python Assignment/API To Postgre/Fetch_data.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_all_pokemon():
+    url = "https://pokeapi.co/api/v2/pokemon?limit=50"
+    with requests.Session() as session:
+        response = session.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data["results"]
+        else:
+            print("Error fetching Pokémon list:", response.status_code)
+            return []
+
+def get_pokemon_details(url, session):
+    response = session.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "id": data["id"],
+            "name": data["name"],
+            "height": data["height"],
+            "weight": data["weight"],
+            "types": [t["type"]["name"] for t in data["types"]],
+            "abilities": [a["ability"]["name"] for a in data["abilities"]]
+        }
+    else:
+        print("Failed to fetch details")
+        return None
+    
